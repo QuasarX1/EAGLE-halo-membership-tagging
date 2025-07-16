56307abaf084d771cd020a3a9fecbde6a89b3db8
Christopher Rowe <chris.rowe19@outlook.com>
2025-07-16 14:42:43 +0100

    Adds imports to the package.

--- eagle-tag/src/eagle_tag/__init__.py
+++ eagle-tag/src/eagle_tag/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2025-present Christopher Rowe <chris.rowe19@outlook.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from ._loading_snapshots import load_snapshot
+from ._loading_catalogue_data import load_catalogue_data
+from ._loading_catalogue_membership import load_catalogue_membership
+from ._save_aux_file import make_aux_file_path, make_aux_file, save_chunk
