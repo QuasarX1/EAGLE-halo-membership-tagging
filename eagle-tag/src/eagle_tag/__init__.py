3ffbae55388f9ccb06f1daa4430400fea64b9ac0
Christopher Rowe <chris.rowe19@outlook.com>
2025-07-18 16:50:01 +0100

    Adds metadata (previous commit) support import.

--- eagle-tag/src/eagle_tag/__init__.py
+++ eagle-tag/src/eagle_tag/__init__.py
@@ -5,4 +5,4 @@
 from ._loading_snapshots import load_snapshot
 from ._loading_catalogue_data import load_catalogue_data
 from ._loading_catalogue_membership import load_catalogue_membership
-from ._save_aux_file import make_aux_file_path, make_aux_file, save_chunk
+from ._save_aux_file import make_aux_file_path, Metadata, make_aux_file, save_chunk
