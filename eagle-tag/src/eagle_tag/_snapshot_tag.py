f2708574dc49f4cd179f2e3db13678cec056366d
Christopher Rowe <chris.rowe19@outlook.com>
2025-07-29 15:34:06 +0100

    Adds a dataclass for representing snapshot tags.

---
+++ eagle-tag/src/eagle_tag/_snapshot_tag.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2025-present Christopher Rowe <chris.rowe19@outlook.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from dataclasses import dataclass
+
+
+
+@dataclass
+class SnapshotTag:
+    """
+    Snapshot number and redshift string used to label EAGLE snapshots.
+    Format: "999_z999p999"
+
+    Attributes:
+        str number:
+            Default "028"
+        str redshift_tag:
+            Default "z000p000" -> 999.999
+    """
+
+    number: str = "028"
+    redshift_tag: str = "z000p000"
+
+    def __str__(self) -> str:
+        return f"{self.number}_{self.redshift}"
+
+    @property
+    def tag(self) -> str:
+        return str(self)
+    
+    @property
+    def redshift(self) -> float:
+        return float(self.redshift_tag[1:].replace("p", "."))
