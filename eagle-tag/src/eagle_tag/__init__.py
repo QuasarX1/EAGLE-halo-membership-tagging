40a185cd0f9c418d8360ad5ca591f22a37d8b908
Christopher Rowe <chris.rowe19@outlook.com>
2025-09-02 11:51:44 +0100

    Adds new imports to the package.
    New functions for caching catalogue data were added in a previous commit (I forgot to mention them in the commit message).

--- eagle-tag/src/eagle_tag/__init__.py
+++ eagle-tag/src/eagle_tag/__init__.py
@@ -6,7 +6,7 @@ from ._load_data_with_xarray import load_hdf5_files_with_xarray, load_hdf5_patte
 from ._snapshot_tag import SnapshotTag
 from ._eagle_filepaths import EAGLE_Files, EAGLE_Snapshot
 from ._loading_snapshots import load_snapshot
-from ._loading_catalogue_data import load_catalogue
+from ._loading_catalogue_data import load_catalogue, cache_catalogue_as_single_file, clear_catalogue_cache, load_catalogue_cache, check_cache_exists
 from ._loading_catalogue_membership import load_catalogue_membership
 from ._save_aux_file import make_aux_file_path, Metadata, make_aux_file, save_chunk
 from ._reorder_particles import calculate_reorder
