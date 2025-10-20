eec5d306dbb8784e8f5399aee5b92c18dbc68af2
Christopher Rowe <chris.rowe19@outlook.com>
2025-10-20 15:12:17 +0100

    Adds the option to ignore specific part types when loading snapshot data.

--- eagle-tag/src/eagle_tag/_loading_catalogue_membership.py
+++ eagle-tag/src/eagle_tag/_loading_catalogue_membership.py
@@ -9,7 +9,7 @@ import numpy as np
 from ._load_data_with_xarray import load_hdf5_pattern_with_xarray
 from ._eagle_filepaths import EAGLE_Snapshot
 
-def load_catalogue_membership(snapshot: EAGLE_Snapshot) -> dict[str, xr.Dataset|None]:
+def load_catalogue_membership(snapshot: EAGLE_Snapshot, do_gas: bool = True, do_dark_matter: bool = True, do_stars: bool = True, do_black_holes: bool = True) -> dict[str, xr.Dataset|None]:
 
     if not snapshot.has_catalogue_membership:
         raise FileNotFoundError(f"Unable to locate catalogue membership files for {snapshot}.")
@@ -30,8 +30,8 @@ def load_catalogue_membership(snapshot: EAGLE_Snapshot) -> dict[str, xr.Dataset|
     any_data_present = np.any(data_in_files, axis = 0)
 
     return {
-        "PartType0" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType0", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][0]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if any_data_present[0] else None,
-        "PartType1" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType1", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][1]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if any_data_present[1] else None,
-        "PartType4" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType4", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][4]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if any_data_present[4] else None,
-        "PartType5" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType5", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][5]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if any_data_present[5] else None,
+        "PartType0" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType0", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][0]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if do_gas         and any_data_present[0] else None,
+        "PartType1" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType1", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][1]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if do_dark_matter and any_data_present[1] else None,
+        "PartType4" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType4", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][4]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if do_stars       and any_data_present[4] else None,
+        "PartType5" : load_hdf5_pattern_with_xarray(filepath_pattern, "PartType5", ["ParticleIDs", "GroupNumber", "SubGroupNumber"], skip_values = [str(i) for i in range(number_of_files) if not data_in_files[i][5]], dimension_sizes = { "catalogue_membership_particle_index" : None, "box_axis_index" : 3 }, concatenation_dimension_index = 0) if do_black_holes and any_data_present[5] else None,
     }
